03437c48b0cc4e92571a9dbc62ced48daf572539
Anurag A <anurag2798@gmail.com>
2020-01-14 10:31:31 +0530

    Create 53. Maximum Subarray.py

---
+++ DP/53. Maximum Subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        l = len(nums)
+        
+        p = [0] * l
+        n = [0] * l
+        
+        p[0],n[0] = nums[0],nums[0]
+        for i in range(1,l):
+            a = nums[i]
+            v = (a,p[i-1] + a,n[i-1] + a)
+            p[i] = max(v)
+            n[i] = min(v)
+        
+        return max(p)
