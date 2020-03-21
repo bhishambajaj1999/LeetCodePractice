b6ce519eff86df51d6c013e91a22ede0803e1bf7
Anurag A <anurag2798@gmail.com>
2020-03-21 18:37:33 +0530

    Create 200. Number of Islands.py

---
+++ DFS/200. Number of Islands.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        c = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if(grid[i][j] == '1'):
+                    self.dfs(grid, i, j)
+                    c += 1
+        
+        return c
+    
+    def dfs(self,grid,i,j):
+        if(i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1'):
+            return 
+        grid[i][j] = '#'
+        self.dfs(grid, i+1,j)
+        self.dfs(grid, i-1,j)
+        self.dfs(grid, i,j+1)
+        self.dfs(grid, i,j-1)
