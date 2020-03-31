29178d2d2e1486208ba5647a41d86cfe034e0d81
Anurag A <anurag2798@gmail.com>
2020-03-31 22:23:01 +0530

    Create 299. Bulls and Cows.py

---
+++ Random/299. Bulls and Cows.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        secret_map, guess_map = {}, {}
+        bull_count, cow_count = 0, 0
+        for i in range(len(secret)):
+            if secret[i] == guess[i]:
+                bull_count += 1
+            else:
+                if secret[i] in secret_map:
+                    secret_map[secret[i]] += 1
+                else:
+                    secret_map[secret[i]] = 1
+                if guess[i] in guess_map:
+                    guess_map[guess[i]] += 1
+                else:
+                    guess_map[guess[i]] = 1
+        for i in guess_map:
+            if i in secret_map:
+                cow_count += min(secret_map[i], guess_map[i])
+        return '%sA%sB' % (bull_count, cow_count)
