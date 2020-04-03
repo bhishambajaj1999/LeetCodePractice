62237f1fc0d5223f0a808d75bb14300a9cd006d2
Anurag A <anurag2798@gmail.com>
2020-04-03 19:08:36 +0530

    Create 1310. XOR Queries of a Subarray.py

---
+++ Array/1310. XOR Queries of a Subarray.py
@@ -0,0 +1,5 @@
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        for i in range(1,len(arr)):
+            arr[i] ^= arr[i-1]
+        return [arr[i-1] ^ arr[j] if i else arr[j] for i,j in queries]
